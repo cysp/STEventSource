9d4665408b8a6be73d0a71dfa1449b0fd235fa56
Scott Talbot <s@chikachow.org>
2016-04-03 20:09:23 +1000

    Import

---
+++ STEventSource/STEventSource.h
@@ -0,0 +1,19 @@
+//
+//  STEventSource.h
+//  STEventSource
+//
+//  Created by Scott Talbot on 3/04/2016.
+//  Copyright © 2016 Scott Talbot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for STEventSource.
+FOUNDATION_EXPORT double STEventSourceVersionNumber;
+
+//! Project version string for STEventSource.
+FOUNDATION_EXPORT const unsigned char STEventSourceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <STEventSource/PublicHeader.h>
+
+
